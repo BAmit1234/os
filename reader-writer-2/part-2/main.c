28ae0f28630e2404a23c3eb94fc31e422d3d9988
BAmit1234 <49117018+BAmit1234@users.noreply.github.com>
2019-04-07 22:33:06 +0530

    part -2 of reader writer project
    this program will tell u which reader is reading or which writer is writing
    along with sleep function in between
    means there will be interval between reader and writer  operation

---
+++ reader-writer-2/part-2/main.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<time.h>
+
+
+pthread_mutex_t e1,e2;
+int rcount = 0;
+int count=0,n;
+void *reader();
+void *writer();
+
+
+
+int main()
+{
+printf("number of reader and writer :");
+scanf("%d",&n);
+pthread_t readerid[n],writerid[n];
+
+pthread_mutex_init(&e1,NULL);
+pthread_mutex_init(&e2,NULL);
+
+printf("*****************************************************************************************\n");
+printf("|WRITER                             READER                    NUMBER OF READER          |\n");
+printf("*****************************************************************************************\n");
+
+while(count<n){
+pthread_create(&writerid[count],NULL,writer,NULL);
+pthread_create(&readerid[count],NULL,reader,NULL);
+
+
+
+pthread_join(writerid[count],NULL);
+pthread_join(readerid[count],NULL);
+count++;
+}
+
+
+printf("****************************************************************************************\n");
+printf("\n");
+printf("**All Reader And Writer Are Done With There OPeration");
+printf("\n");
+return 0;
+
+}
+void *reader()
+{
+pthread_mutex_lock(&e1);
+
+
+rcount++;
+if(rcount==1)
+{
+    pthread_mutex_lock(&e2);
+}
+pthread_mutex_unlock(&e1);
+
+printf("|                           | Data read by the reader %d|                                | \n",count);
+
+Sleep(1000);
+
+pthread_mutex_lock(&e1);
+rcount--;
+if(rcount==0)
+{
+
+    pthread_mutex_unlock(&e2);
+}
+
+pthread_mutex_unlock(&e1);
+
+}
+void *writer()
+{
+
+pthread_mutex_lock(&e2);
+printf("|                           |                          |       Number of reader  %d      |\n",rcount);
+printf("|Data writen by the writer %d|                          |                                |\n",count);
+Sleep(1200);
+
+pthread_mutex_unlock(&e2);
+
+
+}
